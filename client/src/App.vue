aec79634c1ae4b4d2a7b9ccb72d8c9c05b5cb65b
Arduinum628 <message.chaos628@gmail.com>
2026-01-29 15:36:22 +0300

    feat: added clien on vue.js and ionic

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,14 +4,6 @@
   </ion-app>
 </template>
 
-<script>
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-
-export default {
-  name: 'App',
-  components: {
-    IonApp,
-    IonRouterOutlet
-  }
-};
+<script setup>
+import { IonApp, IonRouterOutlet } from '@ionic/vue'
 </script>
