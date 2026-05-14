df2c7e16315f53225f4a5fcd3a12c9a58d65f9f8
Arduinum628 <message.chaos628@gmail.com>
2026-05-14 23:23:57 +0300

    feat: added connected chain ad sound into timer

--- client/src/composables/useTimer.ts
+++ client/src/composables/useTimer.ts
@@ -1,30 +1,73 @@
 import { ref, computed } from 'vue';
 import { CountdownOptions, CountdownReturn } from '@/types/timerType';
+import { stopSound } from '@/composables/useAudio';
+import { TypeTimer } from '@/types/timerType';
+
 
 /** Composable для управления обратным отсчётом времени */
-export function useCountdown({ seconds, onFinish }: CountdownOptions): CountdownReturn {
-  const totalSeconds = seconds * 60;
+export function useCountdown({ timers, onFinish }: CountdownOptions): CountdownReturn {
+  const totalSeconds = ref<number>(0);
+  const timerNow = ref<TypeTimer | undefined>(timers.shift());
+
+  if (timerNow.value) {
+    totalSeconds.value = timerNow.value.time * 60;
+  }
 
   /** Реактивное оставшееся время в секундах */
-  const timeLeft = ref<number>(totalSeconds);
+  const timeLeft = ref<number>(totalSeconds.value);
 
   /** Флаг активного состояния таймера */
   const isRunning = ref<boolean>(false);
 
   let interval: ReturnType<typeof setInterval> | null = null;
 
-  /** Форматирует оставшееся время в строку ЧЧ:ММ:СС */
+  /** Форматирует оставшееся время в строку ЧЧ:ММ:СС или сообщение о прохождении таймера */
   const formatted = computed<string>(() => {
     const hours = Math.floor(timeLeft.value / 3600);
     const mins = Math.floor((timeLeft.value % 3600) / 60);
     const secs = timeLeft.value % 60;
-    return `${String(hours).padStart(2, '0')}:${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+
+    if (timerNow.value) {
+      return `${String(hours).padStart(2, '0')}:${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+    }
+
+    return "Завершён!";
   });
 
   /** Вычисляет процент прошедшего времени от общего */
   const progress = computed<number>(() => {
-    return ((totalSeconds - timeLeft.value) / totalSeconds) * 100;
+    if (totalSeconds.value === 0) return 0;
+
+    return ((totalSeconds.value - timeLeft.value) / totalSeconds.value) * 100;
   });
+
+  /** Отслеживает завершённость таймера */
+  const isFinished = computed<boolean>(() => {
+    return !timerNow.value || timeLeft.value <= 0;
+  });
+
+  /** Задаёт следующий таймер */
+  const setNextTimer = (): void => {
+    timerNow.value = timers.shift();
+
+    if (timerNow.value) {
+      totalSeconds.value = timerNow.value.time * 60;
+      timeLeft.value = totalSeconds.value;
+    }
+  };
+
+  //** Заканчивает таймер */
+  const finish = (): void => {
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+
+    isRunning.value = false;
+    timeLeft.value = 0;
+
+    onFinish?.();
+  };
 
   /** Запускает таймер */
   const start = (): void => {
@@ -35,20 +78,22 @@ export function useCountdown({ seconds, onFinish }: CountdownOptions): Countdown
       if (timeLeft.value > 0) {
         timeLeft.value--;
       } else {
-        stop();
-        onFinish?.();
+        finish();
       }
     }, 1000);
   }
 
-  /** Останавливает таймер и сбрасывает время до начального */
+  /** Останавливает таймер и сбрасывает время до следующего таймера */
   const stop = (): void => {
     if (interval) {
       clearInterval(interval);
       interval = null;
     }
+
     isRunning.value = false;
-    timeLeft.value = totalSeconds;
+
+    stopSound();
+    setNextTimer()
   }
 
   /** Приостанавливает таймер без сброса оставшегося времени */
@@ -64,8 +109,10 @@ export function useCountdown({ seconds, onFinish }: CountdownOptions): Countdown
   return {
     timeLeft,
     isRunning,
+    isFinished,
     formatted,
     progress,
+    timerNow,
     start,
     stop,
     pause
